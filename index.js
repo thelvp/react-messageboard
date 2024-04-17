7ba2a091494c3d81da70f264260577549c7dc52b
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2024-04-17 10:20:58 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import './style.css';
+import App from './App';
+
+createRoot(document.getElementById('app')).render(<App/>);
+                                                  
