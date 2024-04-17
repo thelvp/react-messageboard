c7b74c4fa6ec4dcda20e22613595cf295606149f
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2024-04-17 10:21:15 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {comments} from './commentData';
+
+import Card from './Card';
+
+function App() {
+  return (  
+    comments.map(comment => <Card commentObject={comment} /> )
+  )
+};
+
+export default App; 
